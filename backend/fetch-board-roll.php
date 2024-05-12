f6e152ffbd071e7e0ecb595a774f0f8c6608aa6e
engrreaz <engrreaz@gmail.com>
2024-05-12 17:19:02 +0600

    xx

--- backend/fetch-board-roll.php
+++ backend/fetch-board-roll.php
@@ -5,13 +5,14 @@ include ('../inc2.php');
 $br = $_POST['br'];
 $st = '';
 $sql0 = "SELECT * FROM students where sccode = '$sccode' and rollno = '$br' ;";
-$result0rt = $conn->query($sql0);
-if ($result0rt->num_rows == 1) {
-    while ($row0 = $result0rt->fetch_assoc()) {
-        $st = $st . $row0["stnameeng"];
-    }
-} else {
-    $st = 'Something went wrong.';
-}
+echo $sql0;
+// $result0rt = $conn->query($sql0);
+// if ($result0rt->num_rows == 1) {
+//     while ($row0 = $result0rt->fetch_assoc()) {
+//         $st = $st . $row0["stnameeng"];
+//     }
+// } else {
+//     $st = 'Something went wrong.';
+// }
 
-echo $st;
+// echo $st;
