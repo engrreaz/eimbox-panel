346d6611da8955ae4c436f109433cbe46c5b716f
engrreaz <engrreaz@gmail.com>
2024-05-12 18:04:53 +0600

    board

--- backend/fetch-board-roll.php
+++ backend/fetch-board-roll.php
@@ -11,7 +11,7 @@ $result0rt = $conn->query($sql0);
 if ($result0rt->num_rows == 1) {
     while ($row0 = $result0rt->fetch_assoc()) {
         $st = $st . $row0["stnameeng"];
-    }
+    } 
 } else {
     $st = 'Something went wrong.';
 }
