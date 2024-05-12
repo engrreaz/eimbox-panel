2a6272c1f1e28e2efa746f0ffb8bf1952f4418da
engrreaz <engrreaz@gmail.com>
2024-05-12 18:10:59 +0600

    inc2

--- backend/inc2.php
+++ backend/inc2.php
@@ -3,7 +3,6 @@
 session_start();
 date_default_timezone_set('Asia/Dhaka');
 
-
 $usr = $_SESSION["user"];
 $userlevel = 'Guest';
 
@@ -17,8 +16,6 @@ $td = date('Y-m-d');
 $cur = date('Y-m-d H:i:s');
 
 //********************************************************************
-
-$exam = 'Test';
 
 $sql0 = "SELECT * FROM usersapp where email='$usr' LIMIT 1";
 $result0 = $conn->query($sql0);
@@ -41,6 +38,8 @@ if ($result0->num_rows > 0) {
     $sccode = 99;
     $userlevel = 'Guest';
 }
+
+
 if ($sccode > 100) {
     $sql0x = "SELECT * FROM scinfo where sccode='$sccode' LIMIT 1";
     $result0x = $conn->query($sql0x);
@@ -67,41 +66,15 @@ if ($sccode > 100) {
         }
     }
 
-    if ($userlevel == 'Administrator' || $userlevel == 'Head Teacher') {
-        if ($scname == '' || $scadd1 == '' || $scadd2 = '' || $ps == '' || $dist == '' || $contact == '' || $logo == '') {
-            header("Location: settingsinstituteinfo.php");
-        } else if ($pack == 0) {
-            header("Location: accountbuypack.php");
-        }
-    }
 
-    if ($userlevel == 'Guest') {
-        //
-        //session_start();
-
-        //header("Location: index.php");
-        $pxx = "We noticed that,<br>You're in under review by you Head Teacher / any Administrator.<br> Contact with your authority. <br><br> <b>OR</b><br>You may change your EIIN information.";
-    }
 }
 
-
-$l = strlen($pth);
-if ($l < 5) {
-    $pth = "https://eimbox.com/images/no-image.png";
-}
 
 
 if ($usr == '') {
     $scname = '';
     $scaddress = '';
-
-
     if ($_SERVER['REQUEST_URI'] != "/index.php") {
         header("Location: index.php");
     }
-
-
-} else {
-
-
 }
