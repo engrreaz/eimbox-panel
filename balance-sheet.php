e55e5a40176ea1acae6e6534be955bdc02c549e9
engrreaz <engrreaz@gmail.com>
2024-07-17 19:18:10 +0600

    expenditure

--- balance-sheet.php
+++ balance-sheet.php
@@ -356,8 +356,8 @@ if ($result0r1->num_rows > 0) {
                         $cntamt = 0;
                         $takain = 0;
                         // $sql0 = "SELECT * FROM financeitem where (sccode=0 or sccode='$sccode')  order by slno;";
-                        // $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and income > 0 and date between '$datefrom' and '$dateto' group by partid order by partid;";
-                        $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and income > 0 and month = '$month' and year = '$year'  and module = 'VOUCHER' and type LIKE 'Income' group by partid order by partid;";
+                        $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and income > 0 and date between '$datefrom' and '$dateto' group by partid order by partid;";
+                        // $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and income > 0 and month = '$month' and year = '$year'  and module = 'VOUCHER' and type LIKE 'Income' group by partid order by partid;";
                         // echo $sql0; 
                         $result0 = $conn->query($sql0);
                         if ($result0->num_rows > 0) {
@@ -412,7 +412,7 @@ if ($result0r1->num_rows > 0) {
                         // $sql0 = "SELECT * FROM financeitem where (sccode=0 or sccode='$sccode')  order by slno;";
                         // $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where (sccode='$sccode' || sccode='$sccodes') and expenditure > 0 and date between '$datefrom' and '$dateto' group by partid order by partid;";
                         $sql0 = "SELECT partid, sum(income) as inco, sum(expenditure) as expe, sum(amount) as taka FROM cashbook where (sccode='$sccode' || sccode='$sccodes') and expenditure > 0 and month = '$month' and year = '$year'  and module = 'VOUCHER' and type LIKE 'Expenditure' group by partid order by partid;";
-                        echo $sql0; 
+                        // echo $sql0; 
                         $result02 = $conn->query($sql0);
                         if ($result02->num_rows > 0) {
                             while ($row0 = $result02->fetch_assoc()) {
