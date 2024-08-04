4bc2c9aae7c2d3d33e7eed084c725aace0a775ef
engrreaz <engrreaz@gmail.com>
2024-08-04 16:10:20 +0600

    COMMIT-PUSH

--- monthly-audit-report.php
+++ monthly-audit-report.php
@@ -316,6 +316,7 @@ if ($result0r1->num_rows > 0) {
 
 // Others Income
 $sql0 = "SELECT * FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and partid!=6 and type='Income' and date between '$datefrom' and '$dateto';";
+echo $sql0;
 $result0r2 = $conn->query($sql0);
 if ($result0r2->num_rows > 0) {
     while ($row0 = $result0r2->fetch_assoc()) {
