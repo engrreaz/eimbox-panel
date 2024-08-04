75235c261c1e6e9c2f320e93ae07f914f2dff754
engrreaz <engrreaz@gmail.com>
2024-08-04 16:01:27 +0600

    COMMIT-PUSH

--- monthly-audit-report.php
+++ monthly-audit-report.php
@@ -314,6 +314,27 @@ if ($result0r1->num_rows > 0) {
 }
 
 
+// Others Income
+$sql0 = "SELECT * FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and partid>6 and date between '$datefrom' and '$dateto';";
+$result0r2 = $conn->query($sql0);
+if ($result0r2->num_rows > 0) {
+    while ($row0 = $result0r2->fetch_assoc()) {
+        $date = $row0["date"];
+        $amt = $row0["amount"];
+        $type = $row0["type"];
+        $type = $row0["particulars"];
+
+        $in1 = $in2 = $in3 = $out1 = $out2 = $out3 = 0;
+       
+        $block = 'UNDEF';
+
+        $insins = "INSERT INTO audit_temp (id, cashbook_id, sccode, month, year, type, date, particular, institute_in, govt_in, eduboard_in, institute_out, govt_out, bank_out, amount, block) 
+                        VALUES (NULL, 0, '$sccode', '$month', '$year', '$type', '$date', '$particul', '$in1', '$in2', '$in3', '$out1', '$out2', '$out3', '$amt', '$block')";
+        $conn->query($insins);
+    }
+}
+
+
 // Expenses..................................................
 $sql0 = "SELECT date, category, particulars, refno, sum(amount) as amount FROM cashbook where  (sccode='$sccode' || sccode='$sccodes')  and type='Expenditure' and month = '$month' and year='$year' and partid !=6 group by refno order by date;";
 
