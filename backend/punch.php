efd5eaea0b2275973c0cfed9cc67acc449801e42
Reazul Hoque <engrreaz@gmail.com>
2024-10-23 02:00:52 +0600

    BAF

--- backend/punch.php
+++ backend/punch.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Dhaka');
 include ('inc2.php');
-$td = '2024-10-22';
+// $td = '2024-10-22';
 
 $query331 = "UPDATE stpr SET prno = prno+1 where sccode='$sccode' and prdate='$td'";
 $conn->query($query331);
